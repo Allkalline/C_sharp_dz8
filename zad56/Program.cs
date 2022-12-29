df77a096000b7cb499ecfa072c43c3e5a210457a
VakhrushevDmitry <dimavah@inbox.ru>
2022-12-30 00:12:08 +0300

    это было сложно

---
+++ zad56/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
+
+int[,] GetArray (int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int [m,n];
+    for(int i=0; i<m;i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minValue,maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0);i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+           Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GetArray(4,4,0,5);
+PrintArray(array);
+Console.WriteLine();
+
+
+void Result (int [,] inArray)
+{   int sum = 0;
+    int count = 0;
+    int minSum = 0;
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        minSum = minSum + inArray[0,j];
+    }
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+        
+        {
+            for (int j = 0; j < inArray.GetLength(1); j++)
+                {
+                    sum = sum + inArray[i,j];  
+                }
+                
+                if (minSum > sum) 
+                {
+                    minSum = sum;  
+                    count=i;
+                }
+                
+                sum=0;
+        }
+        Console.WriteLine($"номер строки с наименьшей суммой элементов: {count+1}");
+
+}
+Console.WriteLine();
+Result(array);
---
+++ zad58/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */ 
+
+int[,] GetArray (int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int [m,n];
+    for(int i=0; i<m;i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j] = new Random().Next(minValue,maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0);i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+           Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GetArray(3,3,0,5);
+int[,] array2 = GetArray(3,3,0,5);
+int[,] arrayRes = new int [3,3];
+PrintArray(array);
+Console.WriteLine();
+PrintArray(array2);
+Console.WriteLine();
+
+void Result(int[,] Arr1, int[,] Arr2, int[,] resArr)
+{
+  for (int i = 0; i < resArr.GetLength(0); i++)
+  {
+    for (int j = 0; j < resArr.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < Arr1.GetLength(1); k++)
+      {
+        sum += Arr1[i,k] * Arr2[k,j];
+      }
+      resArr[i,j] = sum;
+    }
+  }
+}
+
+Result (array,array2,arrayRes);
+PrintArray(arrayRes);
